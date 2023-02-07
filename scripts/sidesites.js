1c6e0a56b825a042208ced1e85d63a83e371ae6f
Mundenius <vsaldias97@gmail.com>
2023-02-07 10:22:27 -0300

    apparently it did not upload this

---
+++ scripts/sidesites.js
@@ -0,0 +1,29 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// function for hamburger menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+  navbarLinks.classList.toggle('active')
+
+})
